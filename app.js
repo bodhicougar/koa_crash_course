5a75c97c35e30d7fb217e19aaeea46a47ae3d655
bodhicougar <github@hoosnagh.us>
2018-09-23 22:05:39 +0200

    Hurz
    Signed-off-by: bodhicougar <github@hoosnagh.us>

---
+++ app.js
@@ -0,0 +1,71 @@
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const json = require('koa-json');
+const path = require('path');
+const render = require('koa-ejs');
+const bodyParser = require('koa-bodyparser');
+
+const app = new Koa();
+const router = new KoaRouter();
+
+// Replace with DB
+
+const things = ['My family', 'Programming', 'Music', 'HCF', 'Schnieffielein'];
+
+// JSON Prettier Middleware
+
+app.use(json());
+
+// BodyParser Middleware
+
+app.use(bodyParser());
+
+// Add additional properties to context
+app.context.user = 'Walt';
+
+// Simple Middleware Example
+
+// app.use(async ctx => (ctx.body = { msg: 'Hello World', }));
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: false,
+});
+
+// List of things
+async function index(ctx) {
+  await ctx.render('index', {
+    title: 'Things I Love:',
+    things,
+  });
+}
+
+// Add thing
+async function add(ctx) {
+  const body = ctx.request.body;
+  things.push(body.thing);
+  ctx.redirect('/');
+}
+
+// Show add page
+async function showAdd(ctx) {
+  await ctx.render('add');
+}
+
+// Routes
+
+router.get('/', index);
+router.get('/add', showAdd);
+router.post('/add', add);
+
+router.get('/test', ctx => (ctx.body = `Hello ${ctx.user}`));
+router.get('/test2/:name', ctx => (ctx.body = `Hello ${ctx.params.name}`));
+
+// Router Middleware
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => console.log('Server Started...'));
